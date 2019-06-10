48ebf0442f399696b58fbca549633a229039b74a
Василий Бардюков <ferisefbns@gmail.com>
2019-06-10 16:46:49 +0300

    Fix stream description

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,9 @@ import ManageButton from './components/ManageButton';
 import './layouts/Scrollbar.css';
 import './layouts/ManageBtn.css';
 
+const streamDescription = document.querySelector('.bulletin-board p');
+streamDescription.innerHTML = streamDescription.innerText;
+
 const element = document.createElement('div');
 element.classList.add('manage-st');
 document.querySelector('.chat-dialog').append(element);
