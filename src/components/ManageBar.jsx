b9e369301d1edb5efc354cceba01929843808697
Василий Бардюков <ferisefbns@gmail.com>
2019-08-29 20:08:58 +0300

    Fix text align

--- src/components/ManageBar.jsx
+++ src/components/ManageBar.jsx
@@ -5,16 +5,20 @@ import HideLikesAnimation from './HideLikeAnimation';
 import AutoLikes from './AutoLike';
 import AutoOpenChest from './AutoOpenChest';
 
+const styles = {
+  textAlign: 'left',
+};
+
 const ManageBar = () => (
   <div className="manage-bar" style={{ userSelect: 'none' }}>
     <h4>StreamCraft Settings</h4>
-    <div className="manage-list">
+    <div className="manage-list" style={styles}>
       <TheatreMode />
       <HideContributionGuard />
       <HideLikesAnimation />
     </div>
     <h4>Clickers</h4>
-    <div className="manage-list">
+    <div className="manage-list" style={styles}>
       <AutoLikes />
       <AutoOpenChest />
     </div>
